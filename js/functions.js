5836f1b538edde9d300d876f3ce79e492200588c
migmuni <MiguelAMuniz641@gmail.com>
2016-11-12 14:24:49 -0600

    Optimized images, Updated Fonts, Updated Icon Background
    Images are more optimized to save space, using local FontAwesome
    instead of script, repositioned the background to center instead of top
    left.

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,6 @@
+// Set up Parallax
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
+
+// Fade in page
+window.onload = function () { document.body.className = "";}
