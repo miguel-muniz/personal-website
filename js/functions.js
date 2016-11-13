afd42991cd7a858607bcc55f09e2748415bbca78
migmuni <MiguelAMuniz641@gmail.com>
2016-11-13 13:13:49 -0600

    Updated Responsive Layout and Disabled Background
    Updated the layout to be more responsive, even at extreme sizes.
    Disabled the icon background for now because of responsive issues.

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
+// Fade in page
+window.onload = function () { document.body.className = "";}
+
 // Set up Parallax
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
-
-// Fade in page
-window.onload = function () { document.body.className = "";}
