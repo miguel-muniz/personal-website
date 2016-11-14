fc7c788fb8ac3867d998fe7a206cfaa9bc89e5ca
migmuni <MiguelAMuniz641@gmail.com>
2016-11-14 13:43:39 -0600

    Updated Structure and Footer
    Updated the structure of the right side for a more semantic footer.

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,24 @@
-// Fade in page
-window.onload = function () { document.body.className = "";}
-
 // Set up Parallax
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
+
+window.onload = function(){
+	// Shows page
+	document.body.className = "";
+
+	// Replaces backup footer description with a random description
+	var number = Math.floor(Math.random()*10);
+	var descriptions = [
+		"a pencil nerd.",
+		"an avid Smash Bros. fan.",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"10"
+	];
+	document.getElementById('description').textContent = descriptions[number];
+};
