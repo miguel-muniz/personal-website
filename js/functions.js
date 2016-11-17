29cd1e798edbce6585585dceb9a8f2b5f041203f
migmuni <MiguelAMuniz641@gmail.com>
2016-11-17 14:33:21 -0600

    Completed Footer
    Completed the footer with the random description and correlated icon

--- js/functions.js
+++ js/functions.js
@@ -10,15 +10,42 @@ window.onload = function(){
 	var number = Math.floor(Math.random()*10);
 	var descriptions = [
 		"a pencil nerd.",
-		"an avid Smash Bros. fan.",
-		"3",
-		"4",
-		"5",
-		"6",
-		"7",
-		"8",
-		"9",
-		"10"
+		"a Super Smash Bros. fan.",
+		"a burrito aficionado.",
+		"a rounded typeface connoisseur.",
+		"an icon pattern lover.",
+		"an internet addict.",
+		"a pen tool expert.",
+		"a Pokemon collector.",
+		"a technology buff.",
+		"a YouTube nut."
 	];
+	var descriptionsSrc = [
+		"img/icn-pencil.svg",
+		"img/icn-smash.svg",
+		"img/icn-burrito.svg",
+		"img/icn-type.svg",
+		"img/icn-pattern.svg",
+		"img/icn-internet.svg",
+		"img/icn-pen.svg",
+		"img/icn-pokeball.svg",
+		"img/icn-laptop.svg",
+		"img/icn-video.svg"
+	];
+	var descriptionsAlt = [
+		"Pencil icon",
+		"Super Smash Bros. Ball icon",
+		"Burrito icon",
+		"Rounded letter \"M\" icon",
+		"Icon pattern icon",
+		"Internet globe icon",
+		"Pen tool icon",
+		"Pokeball icon",
+		"Laptop icon",
+		"Video window icon"
+	];
+
 	document.getElementById('description').textContent = descriptions[number];
+	document.getElementById('description-icon').src = descriptionsSrc[number];
+	document.getElementById('description-icon').alt = descriptionsAlt[number];
 };
