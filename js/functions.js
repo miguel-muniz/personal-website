959cf5ee4e9fe38e8af1f60563cceec1fdb4b841
migmuni <MiguelAMuniz641@gmail.com>
2016-11-21 14:05:01 -0600

    Updated project links
    Updated the portfolio sections into links and created the template file

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,7 @@ window.onload = function(){
 	// Shows page
 	document.body.className = "";
 
-	// Replaces backup footer description with a random description
+	// Replaces backup footer description and image with a random description and image
 	var number = Math.floor(Math.random()*10);
 	var descriptions = [
 		"a pencil nerd.",
@@ -44,8 +44,15 @@ window.onload = function(){
 		"Laptop icon",
 		"Video window icon"
 	];
-
 	document.getElementById('description').textContent = descriptions[number];
 	document.getElementById('description-icon').src = descriptionsSrc[number];
 	document.getElementById('description-icon').alt = descriptionsAlt[number];
 };
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-87603739-1', 'auto');
+ga('send', 'pageview');
