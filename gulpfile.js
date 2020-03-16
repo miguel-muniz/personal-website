55d4e290e56d2ef62b038d11c7911be05cb6cc66
Miguel Muniz <miguelamuniz641@gmail.com>
2020-03-15 19:45:35 -0500

    more build configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-var { gulp, src, dest, series } = require('gulp');
-var browserSync = require('browser-sync').create();
+var { src, dest, series } = require('gulp');
 var exec = require('child_process').exec;
+var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
+var cssimport = require('postcss-import');
+var tailwindcss = require('tailwindcss');
+var cssnested = require('postcss-nested');
 
+// Initializes browser sync and serves files
 function serve(cb) {
 	browserSync.init({
 		watch: true,
@@ -11,7 +16,29 @@ function serve(cb) {
 	});
 }
 
+// Builds all the styles using post-css
+function styles(cb) {
+	var plugins = [
+		cssimport(),
+		tailwindcss(),
+		cssnested()
+	];
+	return src('./src/styles/*.css')
+		.pipe(postcss(plugins))
+		.pipe(dest('./dist/styles'));
+}
 
+// Compiles all scripts
+function scripts(cb) {
+
+}
+
+// Optimizes and copies over all media
+function media(cb) {
+
+}
+
+// Builds the website using eleventy
 function build(cb) {
 	exec('npx @11ty/eleventy --input=src --output=dist', function (err, stdout, stderr) {
 		console.log(stdout);
@@ -20,6 +47,10 @@ function build(cb) {
 	});
 }
 
+// Exporting tasks
 exports.build = build;
 exports.serve = serve;
-exports.default = series(build);
+exports.styles = styles;
+exports.scripts = scripts;
+exports.media = media;
+exports.default = series(build, styles);
