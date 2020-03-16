0e909b94cf3a02dcaf41d9543316299575e770df
Miguel Muniz <miguelamuniz641@gmail.com>
2020-03-16 00:16:54 -0500

    add scripts tasks and alpine

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-var { src, dest, series } = require('gulp');
+var { src, dest, series, parallel, task, watch } = require('gulp');
 var exec = require('child_process').exec;
 var browserSync = require('browser-sync').create();
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
 var postcss = require('gulp-postcss');
 var tailwindcss = require('tailwindcss');
 var cssimport = require('postcss-import');
@@ -17,6 +19,7 @@ function serve(cb) {
 			baseDir: "dist"
 		}
 	});
+	cb();
 }
 
 // Builds all the styles using post-css
@@ -31,20 +34,25 @@ function styles(cb) {
 	return src('./src/styles/main.css')
 		.pipe(postcss(plugins))
 		.pipe(dest('./dist/styles'));
+	cb();
 }
 
 // Compiles all scripts
 function scripts(cb) {
-
+	return src('src/scripts/**/*.js')
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(dest('dist/scripts'));
+	cb();
 }
 
 // Optimizes and copies over all media
 function media(cb) {
-
+	cb();
 }
 
 // Builds the website using eleventy
-function build(cb) {
+function eleventy(cb) {
 	exec('npx @11ty/eleventy --input=src --output=dist', function (err, stdout, stderr) {
 		console.log(stdout);
 		console.log(stderr);
@@ -52,10 +60,18 @@ function build(cb) {
 	});
 }
 
+// Watches for changes and runs the appropriate task
+function compile(cb) {
+	watch('src/styles/**/*.css', styles);
+	watch('src/**/*.{html, md, 11ty.js, liquid, njk, hbs, mustache, ejs, haml, pug, jstl}', eleventy);
+	cb();
+}
+
 // Exporting tasks
-exports.build = build;
+exports.eleventy = eleventy;
 exports.serve = serve;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.media = media;
-exports.default = series(build, styles);
+exports.compile = compile;
+exports.default = series(eleventy, styles, parallel(compile, serve));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+	theme: {
+		screens: {
+			xxs: '240px',
+			xs: '480px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			xxl: '1920px',
+		},
+		// Colorscheme: https://www.colorbox.io/#steps=10#hue_start=248#hue_end=232#hue_curve=easeOutSine#sat_start=0#sat_end=50#sat_curve=easeOutSine#sat_rate=120#lum_start=100#lum_end=10#lum_curve=easeOutSine#minor_steps_map=0
+		colors: {
+			transparent: 'transparent',
+			black: '#000',
+			white: '#fff',
+			key: '#0a0c1a',
+			gray: {
+				'100': '#f2f1fe',
+				'200': '#e5e3fd',
+				'300': '#c5c3f5',
+				'400': '#a0a0e6',
+				'500': '#7d81d0',
+				'600': '#5d64b3',
+				'700': '#424a8f',
+				'800': '#2c3368',
+				'900': '#1a1f40',
+			}
+		}
+	},
 }
